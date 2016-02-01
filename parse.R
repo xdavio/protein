05dce0851a01dfe33de3d10761d046b3ada68228
xdavio <xdavio@gmail.com>
2016-01-31 22:16:18 -0600

    new changes for bret. these changes allow the Rfile to read in the information from debug.xlsx as well as the json raw pariwise differenes.

--- parse.R
+++ parse.R
@@ -1,2 +1,13 @@
+#install.packages("jsonlite")
 require(jsonlite)
+#install.packages('gdata')
+require(gdata)
+
+#read in the raw pairs
 pairs = fromJSON("pairs.json")
+
+#read in the features from debug.xlsx
+df = read.xls('debug.xlsx', sheet = 'datadays', header = T)
+
+
+
