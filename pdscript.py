ce68f723d18ffc937da8ece060323dfc9bf12d77
xdavio <xdavio@gmail.com>
2016-02-24 17:34:50 -0600

    update click commands

--- pdscript.py
+++ pdscript.py
@@ -1,5 +1,11 @@
 import click
 from main import allxml
+import pandas as pd
+import xlsxwriter
+
+@click.group()
+def cli():
+    pass
 
 @click.command()
 @click.option('--hush', default = False, is_flag = True)
@@ -18,6 +24,25 @@ def diffmeas(filename, hush):
     if not hush:
         click.echo('The diffmeas follows:')
         click.echo(diff.pairdiff)
+    
+cli.add_command(diffmeas)
 
+
+@click.command()
+@click.argument('filename')
+def explodesheets(filename):
+    """
+    Explodes an excel spreadsheets' many sheets into new excel spreadsheets, each in its own xlsx file. This will use the original spreadsheet name and append the sheet number to the end. 
+    """
+    data = pd.read_excel(filename, sheetname = None)
+    foo = filename.split('.')
+    base = foo[0]
+    ext = foo[1]
     
-    
+    for key, val in data.iteritems():
+        name = base + '_' + str(key) + '.' + ext
+        writer = pd.ExcelWriter(name, engine='xlsxwriter')
+        val.to_excel(writer)
+        writer.save()
+
+cli.add_command(explodesheets)
